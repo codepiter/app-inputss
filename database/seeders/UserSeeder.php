74e3365c4f78871266de33c374a6cd2ba337a816
psanchez <psanchez@zafirus.tech>
2022-04-26 16:54:34 -0400

    Correction in seed to encrypt the credential and be able to access the system without problems

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -17,7 +19,9 @@ class UserSeeder extends Seeder
         $user = new User();
  
         $user->email = "codepiter@gmail.com";
-        $user->password = "22222222";
+       // $user->password = "22222222";
+        $user->password = Hash::make('22222222');
+        //'password' => Hash::make('password'),
         $user->nimda_si = 1;
         $user->save();
     }
